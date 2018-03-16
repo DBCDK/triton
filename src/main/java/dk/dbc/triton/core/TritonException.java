c03373fdf957555755e7c0b2a84be0eb9242a323
Jan Bauer Nielsen <jbn@dbc.dk>
2018-03-16 08:19:06 +0100

    Add DEFAULT_COLLECTION environment variable

--- src/main/java/dk/dbc/triton/core/TritonException.java
+++ src/main/java/dk/dbc/triton/core/TritonException.java
@@ -6,6 +6,14 @@
 package dk.dbc.triton.core;
 
 public class TritonException extends RuntimeException {
+    public TritonException(String message) {
+        super(message);
+    }
+
+    public TritonException(String message, Throwable e) {
+        super(message, e);
+    }
+
     public TritonException(Throwable e) {
         super(e);
     }
